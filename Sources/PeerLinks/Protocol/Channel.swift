2042f47804c5255990558f975a2d3561eacfffa8
Fedor Indutny <fedor@indutny.com>
2019-09-26 01:02:34 -0700

    channel: encryption key

--- Sources/PeerLinks/Protocol/Channel.swift
+++ Sources/PeerLinks/Protocol/Channel.swift
@@ -7,16 +7,24 @@ public class Channel {
   let debugID: String
   let id: Bytes
 
+  private let encryptionKey: Bytes
+
   static let ID_SIZE = 32
   static let ID_KEY = "peerlinks-channel-id".bytes
+  static let ENC_KEY = "peerlinks-symmetric".bytes
 
   init(sodium: Sodium, publicKey: Bytes, name: String) {
     self.sodium = sodium
     self.publicKey = publicKey
     self.name = name
-    self.id = sodium.genericHash.hash(message: publicKey,
-                                      key: Channel.ID_KEY,
-                                      outputLength: Channel.ID_SIZE)!
+    id = sodium.genericHash.hash(message: publicKey,
+                                 key: Channel.ID_KEY,
+                                 outputLength: Channel.ID_SIZE)!
+
+    encryptionKey = sodium.genericHash.hash(
+        message: publicKey,
+        key: Channel.ENC_KEY,
+        outputLength: sodium.secretBox.KeyBytes)!
 
     debugID = "\(Debug.toID(sodium: sodium, publicKey: publicKey))/\(name)"
   }
